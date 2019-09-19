e2241564f6d2859de5fb76c1fd66682294e4d773
Buonanno <grasta4@gmail.com>
2019-09-19 16:29:58 +0200

    C# version

---
+++ C#/src/main/FileManager.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace TesterTools
+{ 
+    static class FileManager
+    {
+        public static void ClearDir(string dirPath)
+        {
+            DirectoryInfo di = new DirectoryInfo(dirPath);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+        }
+
+        public static void WriteToFile(string file, string line, bool? newLineBegin, bool semicolon)
+        {
+            string tmp = line += (semicolon ? ";" : "");
+
+            if (newLineBegin != null)
+                tmp = (bool)newLineBegin ? "\n" + line + (semicolon ? ";" : "") : line + (semicolon ? ";\n" : "\n");
+
+            using (var tw = new StreamWriter(file, true))
+                tw.Write(line);
+        }
+    }
+}
---
+++ C#/src/main/SolverEnum.cs
@@ -0,0 +1,37 @@
+﻿using it.unical.mat.embasp.platforms.desktop;
+using it.unical.mat.embasp.specializations.clingo.desktop;
+using it.unical.mat.embasp.specializations.dlv.desktop;
+using System;
+
+namespace TesterTools
+{
+    static class SolverEnum
+    {
+        public const string CLINGO = "clingo", DLV = "dlv";
+
+        public static string GetOutputOption(string solver)
+        {
+            switch (solver)
+            {
+                case CLINGO: return "--verbose=0";
+                case DLV: return "-silent";
+                default: Environment.Exit(exitCode: -1); return null;
+            }
+        }
+
+        public static DesktopService GetService(string solver)
+        {
+            switch (solver)
+            {
+                case CLINGO: return new ClingoDesktopService("../../rsc/" + solver + ".solver");
+                case DLV: return new DLVDesktopService("../../rsc/" + solver + ".solver");
+                default: Environment.Exit(exitCode: -1); return null;
+            }
+        }
+
+        public static string[] GetSolvers()
+        {
+            return new string[] { CLINGO, DLV };
+        }
+    }
+}
